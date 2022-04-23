94ba2d168e09a58a9025589ea99af680eade94c7
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-04-23 18:12:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const firstBase = 0;
+let totalBatteries = batteryBatches.reduce(
+  (param1, param2) => param1 + param2,
+  firstBase
+);
+
+console.log(totalBatteries);
